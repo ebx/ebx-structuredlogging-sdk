f4fbee9447f573ac52e3da41a4f1e245452cb124
Dougal Rea <dougal.rea@echobox.com>
2023-05-03 10:46:25 +0100

    SL-5990 Roll out CircleCI deployment infrastructure to structuredlogging-sdk

--- Infrastructure/Build-Scripts/validate_build.sh
+++ Infrastructure/Build-Scripts/validate_build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+# shellcheck disable=SC2059
 ##
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -16,24 +18,8 @@
 # limitations under the License.
 ##
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  printf "${GREEN_COLOUR}Building base branch $SOURCE_BRANCH_NAME which is at version $MVN_VERSION.${NO_COLOUR}\n"
+if [ "${CIRCLE_BRANCH}" == "${RELEASE_BRANCH}" ] || [ "${CIRCLE_BRANCH}" == "${DEV_BRANCH}" ]; then
+  printf "${GREEN_COLOUR}Building base branch $CIRCLE_BRANCH.${NO_COLOUR}\n"
 else
-  #Get the PR title
-  export PR_TITLE=$(curl -s https://api.github.com/repos/$REPO/pulls/$TRAVIS_PULL_REQUEST | grep -Po '(?<="title":[[:space:]]")[^"]*(?=",)')
-
-  printf "${GREEN_COLOUR}Building PR #$TRAVIS_PULL_REQUEST '$PR_TITLE' from branch $SOURCE_BRANCH_NAME (into $TRAVIS_BRANCH)${NO_COLOUR}\n"
-  
-  ##Disabled as travis keeps getting rate limited by github
-  ##Ensure the PR name matches our expected format
-  #if ! [[ $PR_TITLE =~ ^[A-Z]{2,4}-[0-9]+[[:space:]].+$ ]]; then
-  #  printf "${RED_COLOUR}PR title '$PR_TITLE' does not match the expected format 'GH-[xxxx] [description]'.${NO_COLOUR}\n"
-  #  travis_terminate 1;
-  #fi
-  
-  #Ensure PR cannot be into master, unless it's coming from dev
-  if [ "$TRAVIS_BRANCH" == "$RELEASE_BRANCH" ] && [ "$SOURCE_BRANCH_NAME" != "$DEV_BRANCH" ]; then
-    printf "${RED_COLOUR}Build failed as PR target is master. Please ensure you use $DEV_BRANCH as the target.${NO_COLOUR}\n"
-    travis_terminate 1;
-  fi
+  printf "${GREEN_COLOUR}Building PR #${CIRCLE_PULL_REQUEST##*/} '${CIRCLE_PULL_REQUEST}' from branch ${CIRCLE_BRANCH} (into ${DEV_BRANCH})${NO_COLOUR}\n"
 fi
